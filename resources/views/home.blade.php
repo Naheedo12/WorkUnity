a63f32f352b79bf6cb198a4dc24474b507d241ea
Naheedo12 <elqadisalma1@gmail.com>
2025-10-22 08:47:55 +0100

    add tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Home Page</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1>Page home</h1>
-    <a href="{{ route('Ajouter') }}">Ajouter</a>
+
+        <h1 class="text-4xl font-bold mb-6 text-blue-600">Page Home</h1>
+        <a href="{{ route('Ajouter') }}" 
+           class="inline-block bg-blue-500 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-600 transition duration-300">
+            Ajouter
+        </a>
+
 </body>
 </html>
