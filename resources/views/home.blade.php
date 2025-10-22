a72c166ffdc2d1be30b017d8bdccafe1c547c64c
Naheedo12 <elqadisalma1@gmail.com>
2025-10-22 11:18:13 +0100

    modify on tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Page</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+    @vite('resources/css/app.css')
 </head>
 <body>
 
-        <h1 class="text-4xl font-bold mb-6 text-blue-600">Page Home</h1>
+        <h1 class="text-4xl font-bold mb-6 text-pink-600">Page Home</h1>
         <a href="{{ route('Ajouter') }}" 
-           class="inline-block bg-blue-500 text-white px-6 py-3 rounded-lg shadow hover:bg-blue-600 transition duration-300">
+           class="inline-block bg-pink-500 text-white px-6 py-3 rounded-lg shadow hover:bg-green-600 transition duration-300">
             Ajouter
         </a>
 
